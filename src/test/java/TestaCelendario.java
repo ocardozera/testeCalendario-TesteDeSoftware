14e32c2d0da09b4a5a92785e9e924a92122b6293
Matheus Cardoso <matheusjosecardoso@outlook.com>
2022-05-30 21:55:06 -0300

    Testes aula 30/05/2022 pré-avaliação

--- src/test/java/TestaCelendario.java
+++ src/test/java/TestaCelendario.java
@@ -53,4 +53,57 @@ public class TestaCelendario {
 //
 //        assertEquals("mostrarCalendario: @: ano inválido.", excecao.getMessage());
 //    }
+
+
+    ///TESTES PEDIDOS PELO PROFESSOR
+
+    //Teste 01 - Mês não-inteiro, ano não-inteiro
+    @Test
+    public void testarMesNaoInteiroAnoNaoInteiro() throws IOException {
+        Path arquivo = Paths.get("src\\test\\resources", "janeiro2022.txt");
+        String janeiro2022 = Files.readString(arquivo); //saida esperada
+        Calendario.mostrarCalendario("1,5", "2,2");
+        String obtido = saidaConsole.toString();
+        assertEquals(janeiro2022.replaceAll("\\s+", ""), obtido.replaceAll("\\s+", ""));
+    }
+
+    //Teste 02 - Mês não-inteiro, ano < 1
+    @Test
+    public void testarMesNaoInteiroAnoMenorQueUm() throws IOException {
+        Path arquivo = Paths.get("src\\test\\resources", "janeiro2022.txt");
+        String janeiro2022 = Files.readString(arquivo); //saida esperada
+        Calendario.mostrarCalendario("3,5", "0");
+        String obtido = saidaConsole.toString();
+        assertEquals(janeiro2022.replaceAll("\\s+", ""), obtido.replaceAll("\\s+", ""));
+    }
+
+    //Teste 03 - Mês não-inteiro, ano > 9999
+    @Test
+    public void testarMesNaoInteiroAnoMaiorQue9999() throws IOException {
+        Path arquivo = Paths.get("src\\test\\resources", "janeiro2022.txt");
+        String janeiro2022 = Files.readString(arquivo); //saida esperada
+        Calendario.mostrarCalendario("3,5", "10000");
+        String obtido = saidaConsole.toString();
+        assertEquals(janeiro2022.replaceAll("\\s+", ""), obtido.replaceAll("\\s+", ""));
+    }
+
+    //Teste 04 - Mês não-inteiro, 1 <= ano <= 9999
+    @Test
+    public void testarMesNaoInteiroAnoMenorIgualUmMaiorQueAte9999() throws IOException {
+        Path arquivo = Paths.get("src\\test\\resources", "janeiro2022.txt");
+        String janeiro2022 = Files.readString(arquivo); //saida esperada
+        Calendario.mostrarCalendario("3,5", "2022");
+        String obtido = saidaConsole.toString();
+        assertEquals(janeiro2022.replaceAll("\\s+", ""), obtido.replaceAll("\\s+", ""));
+    }
+
+    //Teste 05 - Mês menor que um, Mês não-inteiro
+    @Test
+    public void testarMesMenorQueUmAnoNaoInteiro() throws IOException {
+        Path arquivo = Paths.get("src\\test\\resources", "janeiro2022.txt");
+        String janeiro2022 = Files.readString(arquivo); //saida esperada
+        Calendario.mostrarCalendario("0", "3,5");
+        String obtido = saidaConsole.toString();
+        assertEquals(janeiro2022.replaceAll("\\s+", ""), obtido.replaceAll("\\s+", ""));
+    }
 }
